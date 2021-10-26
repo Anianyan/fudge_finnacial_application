99711d3ceaf83bb7ba462564cd5100f84d210b41
anianyan <anianyan10@gmail.com>
2021-10-26 09:25:03 +0400

    returned comments in get post & added get user posts endpint

--- src/controllers/post-controller.js
+++ src/controllers/post-controller.js
@@ -1,16 +1,66 @@
+const mongoose = require('mongoose');
+const { isEmpty, map, toLength } = require('lodash');
+
 const { POST_STATUS } = require('../constants');
 const { NotFoundError } = require('../errors');
-const { PostModel, UserModel } = require('../models');
+const { PostModel, UserModel, CommentModel } = require('../models');
 
 async function getPost(req, res, next) {
   try {
     const { postId } = req.params;
-    const post = await PostModel.findById(postId);
-    if (!post) {
+    const post = await PostModel.aggregate([
+      {
+        $match: {
+          _id: mongoose.Types.ObjectId(postId),
+          status: POST_STATUS.PUBLISHED,
+        },
+      },
+      {
+        $lookup: {
+          from: CommentModel.collection.name,
+          localField: '_id',
+          foreignField: 'postId',
+          as: 'comments',
+        },
+      },
+    ]);
+    if (isEmpty(post)) {
       throw new NotFoundError('Post not found');
     }
-    // TODO: here need write aggregation with comments doc.
     return res.json(post);
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function getUserPosts(req, res, next) {
+  try {
+    const { userId } = req.params;
+
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      throw new NotFoundError('User not found');
+    }
+    const data = {
+      user,
+      posts: [],
+    };
+    const posts = await PostModel.find({ userId, status: POST_STATUS.PUBLISHED });
+
+    if (isEmpty(posts)) {
+      return res.json(data);
+    }
+    data.posts = await Promise.all(map(posts, async (post) => {
+      const { _id } = post;
+      const comments = await CommentModel.find({ postId: _id });
+
+      return {
+        ...post.toObject(),
+        comments,
+      };
+    }));
+
+    return res.json(data);
   } catch (error) {
     next(error);
   }
@@ -53,6 +103,7 @@ async function deletePost(req, res, next) {
 }
 
 module.exports = {
+  getUserPosts,
   getPost,
   createPost,
   deletePost,
--- src/routers/post-router.js
+++ src/routers/post-router.js
@@ -3,8 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const { validatePostId, validateCreatePost } = require('../middlewares/validators/post-validator');
-const { getPost, createPost, deletePost } = require('../controllers/post-controller');
+const { validateUserId } = require('../middlewares/validators/user-validator');
+const { getPost, getUserPosts, createPost, deletePost } = require('../controllers/post-controller');
 
+router.get('/user/:userId', validateUserId, getUserPosts);
 router.get('/:postId', validatePostId, getPost);
 router.post('/', validateCreatePost, createPost);
 router.delete('/:postId', validatePostId, deletePost);
